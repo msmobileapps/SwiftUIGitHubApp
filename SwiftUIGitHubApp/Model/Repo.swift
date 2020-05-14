462b9c179f3bf2c7e2e1d205510d6f87ef20a466
Nikita Koniukh <nikita@msapps>
2020-05-14 14:34:22 +0300

    fixed download service

--- SwiftUIGitHubApp/Model/Repo.swift
+++ SwiftUIGitHubApp/Model/Repo.swift
@@ -8,7 +8,8 @@
 
 import SwiftUI
 
-struct Repo {
+class Repo: ObservableObject {
+
     let image: UIImage?
     let name: String
     let description: String
@@ -16,4 +17,16 @@ struct Repo {
     let language: String
     let contributers: Int
     let repoUrl: String
+
+    init(image: UIImage, name: String, description: String, numberOfForks: Int, language: String, contributers: Int, repoUrl: String) {
+        self.image = image
+        self.name = name
+        self.description = description
+        self.numberOfForks = numberOfForks
+        self.language = language
+        self.contributers = contributers
+        self.repoUrl = repoUrl
+    }
 }
+
+
--- SwiftUIGitHubApp/Services/DownloadService.swift
+++ SwiftUIGitHubApp/Services/DownloadService.swift
@@ -15,7 +15,7 @@ class DownloadService {
     static let instance = DownloadService()
     typealias dictionary = Dictionary<String, Any>
 
-    func downloadTrendingRepoDictArray(completion: @escaping (_ reposDictArray: [dictionary]) -> ()) {
+    private func downloadTrendingRepoDictArray(completion: @escaping ([dictionary]) -> ()) {
         var trendingReposArray = [dictionary]()
         AF.request(TRENDING_REPO_URL).responseJSON { (response) in
             switch response.result {
@@ -25,23 +25,21 @@ class DownloadService {
                     else { return }
 
                 for dict in repoDictionaryArray {
-
-                        let name = dict["name"] as? String
-                        let description = dict["description"] as? String
-                        let numberOfForks = dict["forks_count"] as? Int
-                        let language = dict["language"] as? String
-                        let repoUrl = dict["html_url"] as? String
-                        let contributorsUrl = dict["contributors_url"] as? String
-                        let ownerDict = dict["owner"] as? dictionary
-                        let avatarUrl = ownerDict?["avatar_url"] as? String
-
-                    let repoDictioanary: dictionary = ["name": name ?? "", "description": description ?? "", "forks_count": numberOfForks ?? 0, "language": language ?? "", "html_url": repoUrl ?? "", "contributors_url": contributorsUrl ?? "", "avatar_url": avatarUrl ?? ""]
+                    guard let name = dict["name"] as? String,
+                        let description = dict["description"] as? String,
+                        let numberOfForks = dict["forks_count"] as? Int,
+                        let language = dict["language"] as? String,
+                        let repoUrl = dict["html_url"] as? String,
+                        let contributorsUrl = dict["contributors_url"] as? String,
+                        let ownerDict = dict["owner"] as? dictionary,
+                        let avatarUrl = ownerDict["avatar_url"] as? String else {
+                            continue
+                    }
+                    let repoDictioanary: dictionary = ["name": name, "description": description, "forks_count": numberOfForks, "language": language, "html_url": repoUrl, "contributors_url": contributorsUrl, "avatar_url": avatarUrl]
                     trendingReposArray.append(repoDictioanary)
                 }
-
             case .failure(let error):
                 print(error.localizedDescription)
-
             }
             completion(trendingReposArray)
         }
@@ -61,15 +59,16 @@ class DownloadService {
     }
 
 
-
-    func downloadTrendingRepo(fromDict dict: dictionary, completion: @escaping (Repo) -> ()) {
-        let avatarUrl = dict["avatar_url"] as? String ?? ""
-        let name = dict["name"] as? String ?? ""
-        let description = dict["description"] as? String ?? ""
-        let numberOfForks = dict["forks_count"] as? Int ?? 0
-        let language = dict["language"] as? String ?? ""
-        let repoUrl = dict["html_url"] as? String ?? ""
-        let contributorsUrl = dict["contributors_url"] as? String ?? ""
+    private func downloadTrendingRepo(fromDict dict: dictionary, completion: @escaping (Repo) -> ()) {
+        guard let avatarUrl = dict["avatar_url"] as? String,
+            let name = dict["name"] as? String,
+            let description = dict["description"] as? String,
+            let numberOfForks = dict["forks_count"] as? Int,
+            let language = dict["language"] as? String,
+            let repoUrl = dict["html_url"] as? String,
+            let contributorsUrl = dict["contributors_url"] as? String else {
+                return
+        }
 
         self.downloadContributorsData(for: contributorsUrl) { (contributions) in
             self.downloadImage(for: avatarUrl) { (image) in
@@ -80,19 +79,19 @@ class DownloadService {
         }
     }
 
-    func downloadImage(for avatarUrl: String, completion: @escaping(UIImage) -> ()){
-        
+    private func downloadImage(for avatarUrl: String, completion: @escaping(UIImage) -> ()){
+        guard let placeHolder = UIImage(named: "image_placeholder") else { return }
         AF.request(avatarUrl).responseImage { (imageResponse) in
             switch imageResponse.result {
             case .success(let image):
                 completion(image)
-            case .failure(let error):
-                print(error)
+            case .failure(_):
+                completion(placeHolder)
             }
         }
     }
 
-    func downloadContributorsData(for contributorsUrl: String, completion: @escaping (_ contributors: Int) -> ()) {
+    private func downloadContributorsData(for contributorsUrl: String, completion: @escaping (_ contributors: Int) -> ()) {
         AF.request(contributorsUrl).responseJSON { (response) in
             switch response.result {
             case .success(let value):
